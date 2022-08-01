c67bf4507beea5a0aaeae6d5fa7b00ebe560b117
Federico <fedeaacevedo@gmail.com>
2022-08-01 15:58:01 -0300

    UsuarioController OK

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,9 @@ import java.util.List;
 
 @RestController
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
 
     @RequestMapping(value = "usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id){
@@ -24,40 +29,9 @@ public class UsuarioController {
 
     @RequestMapping(value = "usuarios")
     public List <Usuario> getUsuarios(){
-        List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(234L);
-        usuario.setNombre("Federico");
-        usuario.setApellido("Acevedo");
-        usuario.setEmail("correo@correo.com");
-        usuario.setTelefono("2214288025");
 
-        Usuario usuario1 = new Usuario();
-        usuario1.setId(2344L);
-        usuario1.setNombre("Ludmila");
-        usuario1.setApellido("Lares");
-        usuario1.setEmail("ludmila@correo.com");
-        usuario1.setTelefono("22142883245");
+        return usuarioDao.getUsuarios();
 
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(345345L);
-        usuario2.setNombre("Felipe");
-        usuario2.setApellido("Acevedo");
-        usuario2.setEmail("felipe@correo.com");
-        usuario2.setTelefono("22142388025");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(456L);
-        usuario3.setNombre("Francesca");
-        usuario3.setApellido("Acevedo");
-        usuario3.setEmail("francesca@correo.com");
-        usuario3.setTelefono("2214234025");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario1);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
     }
 
 
